6214eea37b22d068e29bf14982950b88976934ae
Himanshu Shukla <Himanshu.Shukla@ClickMinds.com>
2024-11-18 15:38:46 +0530

    Initial Commit.

---
+++ UserAuthApi/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserAuthApi
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult Authenticate([FromBody] AuthenticateModel model)
+        {
+            var token = _userService.AuthenticateUser(model.Username, model.Password);
+
+            if (token != null)
+            {
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized();
+        }
+    }
+
+    public class AuthenticateModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
